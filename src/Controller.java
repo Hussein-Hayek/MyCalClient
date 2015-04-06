bc01f7516aafe18bddd4b0e4be8db47e933fa4be
Hussein-Hayek <hussein-hayek@windowslive.com>
2015-04-06 10:29:37 +0300

    editing main class and login page

--- src/Controller.java
+++ src/Controller.java
@@ -1,4 +1,24 @@
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
 
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable {
+
+    @FXML
+    private TextField Login_U;
+    @FXML
+    private PasswordField Login_P;
+    @FXML
+    private void Login() {
+        String out="login\n"+Login_U+"\n"+Login_P+"\n";
+        
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,36 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.*;
+import java.net.*;
 
 public class Main extends Application {
+    private static Socket clientSocket=null;
+    private static boolean connected=false;
+    private static DataOutputStream outToServer=null;
+    private static BufferedReader inFromServer=null;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("MyCal: Login");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception
+    {
+        String response;
+        try {
+            clientSocket= new Socket("hostname",2015);
+            outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // Buffered reader is saving input in a buffer so that when we invoke .readLine() // it will efficiently return everything before .readLine. sentence = inFromUser.readLine();
+            response=inFromServer.readLine();
+            if (response.equals("Welcome")) connected = true;
+        }
+        catch (IOException e) {
+        }
         launch(args);
     }
 }
