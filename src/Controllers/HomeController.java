5d4a00495d9443fec24104891dd53465d309fdff
abdallah95 <abdallah.moussawi@gmail.com>
2015-04-25 12:34:09 +0300

    calendar customizing

--- src/Controllers/HomeController.java
+++ src/Controllers/HomeController.java
@@ -2,12 +2,12 @@ package Controllers;
 
 import Models.MyCalCalendar;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -52,33 +52,38 @@ public class HomeController implements Initializable {
     public TableColumn<MyCalCalendar,String> sunday_c;
 
     private final String[] months={"January","February","March","April","May","June","July","August","September","October","November","December"};
-
+    private ObservableList<Integer> years=FXCollections.observableArrayList();
     @Override
     public void initialize(URL url,ResourceBundle resourceBundle){
-        initializeCalendar();
+        updateCalendar();
+        sunday_c.setCellValueFactory(data -> new SimpleStringProperty("" + (data.getValue().getDate())));
+        monday_c.setCellValueFactory(data -> new SimpleStringProperty("" + (data.getValue().getDate())));
+        tuesday_c.setCellValueFactory(data->new SimpleStringProperty(""+(data.getValue().getDate())));
+        wednesday_c.setCellValueFactory(data->new SimpleStringProperty(""+(data.getValue().getDate())));
+        thursday_c.setCellValueFactory(data->new SimpleStringProperty(""+(data.getValue().getDate())));
+        friday_c.setCellValueFactory(data -> new SimpleStringProperty("" + (data.getValue().getDate())));
+        saturday_c.setCellValueFactory(data -> new SimpleStringProperty("" + (data.getValue().getDate())));
+        year.setItems(years);
     }
 
-    private void initializeCalendar(){
+    private void updateCalendar(){
         initializeUpdateTask();
         (new Thread(update)).start();
+        month.setText(months[currentDate.get(Calendar.MONTH)]);
+        year.setValue(currentDate.get(Calendar.YEAR));
     }
     private void initializeUpdateTask(){
         update=new Task<Void>() {
             @Override
             protected Void call() throws Exception {
                 MyCalCalendar.update();
+                for(int i=1980;i<2100;i++)
+                    years.add(i);
                 return null;
             }
             @Override
             protected void succeeded(){
                 ArrayList<ObservableList<MyCalCalendar>> mycalcalendar=MyCalCalendar.getMonth(currentDate.get(Calendar.MONTH),currentDate.get(Calendar.YEAR));
-                sunday_c.setCellValueFactory(data->new SimpleStringProperty(""+(data.getValue().getDate())));
-                monday_c.setCellValueFactory(data->new SimpleStringProperty(""+(data.getValue().getDate())));
-                tuesday_c.setCellValueFactory(data->new SimpleStringProperty(""+(data.getValue().getDate())));
-                wednesday_c.setCellValueFactory(data->new SimpleStringProperty(""+(data.getValue().getDate())));
-                thursday_c.setCellValueFactory(data->new SimpleStringProperty(""+(data.getValue().getDate())));
-                friday_c.setCellValueFactory(data->new SimpleStringProperty(""+(data.getValue().getDate())));
-                saturday_c.setCellValueFactory(data->new SimpleStringProperty(""+(data.getValue().getDate())));
                 sunday.setItems(mycalcalendar.get(0));
                 monday.setItems(mycalcalendar.get(1));
                 tuesday.setItems(mycalcalendar.get(2));
@@ -86,21 +91,32 @@ public class HomeController implements Initializable {
                 thursday.setItems(mycalcalendar.get(4));
                 friday.setItems(mycalcalendar.get(5));
                 saturday.setItems(mycalcalendar.get(6));
-                month.setText(months[currentDate.get(Calendar.MONTH)]);
-                year.setValue(currentDate.get(Calendar.YEAR));
             }
+
         };
     }
 
     @FXML
     public void addMonth(){
-
+        currentDate.add(Calendar.MONTH,1);
+        updateCalendar();
     }
 
     @FXML
     public void subMonth(){
-
+        currentDate.add(Calendar.MONTH,-1);
+        updateCalendar();
     }
 
+    @FXML
+    public void selectYear(){
+        currentDate.set(Calendar.YEAR,year.getValue());
+        updateCalendar();
+    }
+
+    @FXML
+    public void addEvent(){
+        
+    }
 
 }
